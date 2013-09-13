f840c235ef658313e26680b7e8d912cac6055c6c
Kim Thoenen <kim@smuzey.ch>
2013-09-13 12:56:38 +0200

    Heavy update
    - Now committing after every major step
    - Commit messages contain link to cloned repository at checked out rev
    - added moving of default settings

--- djinit.py
+++ djinit.py
@@ -10,6 +10,7 @@ from getpass import getpass
 
 import os
 from random import choice
+import re
 
 
 if __name__ == '__main__':
@@ -25,6 +26,8 @@ if __name__ == '__main__':
     DIVIO_DJANGO_TEMPLATE = 'git@github.com:divio/divio-django-template.git'
     DIVIO_BOILERPLATE = 'git@github.com:divio/divio-boilerplate.git'
     DIVO_STANDARDSITE = 'git@github.com:divio/divio-standardsite.git'
+    versions = {}
+    pushes = 4
 
     q = " -q"  # quiet flag for git commands
 
@@ -76,7 +79,8 @@ if __name__ == '__main__':
             project_name = input("Please enter the repo name: ")
             repo_owner = input("Please enter the account which the repo belongs to: ")
             while 1:
-                i = input("Is this the repo's Github remote: git@github.com:" + repo_owner + "/" + project_name + ".git? (y/n) ")
+                i = input("Is this the repo's Github remote: git@github.com:" + repo_owner + "/"
+                          + project_name + ".git? (y/n) ")
                 if i == 'y':
                     github_remote = "git@github.com:" + repo_owner + "/" + project_name + ".git"
                     break
@@ -91,6 +95,7 @@ if __name__ == '__main__':
         i = input("Do you want to add the standardsite styles and templates from divio-styleguide? (y/n) ")
         if i == 'y':
             standardsite = True
+            pushes += 1
             break
         elif i == 'n':
             standardsite = False
@@ -116,10 +121,11 @@ if __name__ == '__main__':
     print("* Cloning Divio Django Template")
     # print("* Cloning Divio Django Template...\t"),
     os.system('git clone ' + DIVIO_DJANGO_TEMPLATE + ' ' + pn + q)
+    versions['divio-django-template'] = os.popen('cd ' + pn + ' && git log | head -1').read().strip("commit ").strip("\n")
     print("  OK")
     print("")
 
-    # init-ing git repo
+    # initializing git repo
     print("* Initializing Git repo")
     os.system('rm -rf ' + pn + '/.git')
     os.system('cd ' + pn + ' && git init' + q)
@@ -127,12 +133,35 @@ if __name__ == '__main__':
     print("  OK")
     print("")
 
+    # initial push to github
+    print("* Initial Push to Github")
+    os.system('cd ' + pn + ' && git add -A .')
+    os.system('cd ' + pn + ' && git commit -m "djinit (1/' + str(pushes)
+              + '): Initial commit" -m "push based on divio-django-template (divio/divio-django-template@'
+              + str(versions['divio-django-template']) + ')"' + q)
+    os.system('cd ' + pn + ' && git push -u origin master' + q)
+    print("  OK")
+    print("")
+
+    # make temp dir
+    os.system("mkdir " + pn + "_tmp")
+
     # copying boilerplate
     print("* Copying files from Boilerplate")
     os.system('git clone ' + DIVIO_BOILERPLATE + ' ' + pn + '_tmp/bp' + q)
-    os.system('cd ' + pn + '_tmp/bp')
-    os.system('cp -r ' + pn + '_tmp/bp/sass ' + pn + '_tmp/bp/static ' +
+    versions['divio-boilerplate'] = os.popen('cd ' + pn + '_tmp/bp && git log | head -1').read().strip("commit ").strip("\n")
+    os.system('cp -r ' + pn + '_tmp/bp/private/sass ' + pn + '_tmp/bp/static ' +
               pn + '_tmp/bp/templates ' + pn + '_tmp/bp/config.rb ' + pn + '/')
+    print("  OK")
+    print("")
+
+    # after boilerplate add push to github
+    print("* Added Boilerplate Files, another push to Github")
+    os.system('cd ' + pn + ' && git add -A .')
+    os.system('cd ' + pn + ' && git commit -m "djinit (2/' + str(pushes)
+              + '): Added boilerplate files" -m "push based on divio-boilerplate (divio/divio-boilerplate@'
+              + str(versions['divio-boilerplate']) + ')"' + q)
+    os.system('cd ' + pn + ' && git push -u origin master' + q)
     print("  OK")
     print("")
 
@@ -144,13 +173,29 @@ if __name__ == '__main__':
     f = open(pn + "/src/settings.py", "w")
     for line in lines:
         # removing secret key and exception and comments concerning this
-        if 'SECRET_KEY' not in line and 'Run this on a console to get one' not in line and 'from random import choice; print' not in line:
+        if 'SECRET_KEY' not in line and 'Run this on a console to get one' not in line and\
+                        'from random import choice; print' not in line:
             f.write(line)
 
-    key = ''.join([choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)])
+    key = re.escape("".join([choice("abcdefghijklmnopqrstuvwxyz0123456789^&*(-_=+)") for i in range(50)]))
     f.write("SECRET_KEY = '" + key + "'")
 
     f.close()
+    print("  OK")
+    print("")
+
+    # move settings
+    print("* Moving default settings")
+    os.system('cd ' + pn + ' && mv deployment_default.py deployment.py')
+    os.system('cd ' + pn + '/src && mv settings_server_dev_default.py settings_server_dev.py')
+    print("  OK")
+    print("")
+
+    # after settings setup push to github
+    print("* Set up Settings, another push to Github")
+    os.system('cd ' + pn + ' && git add -A .')
+    os.system('cd ' + pn + ' && git commit -m "djinit (3/' + str(pushes) + '): Set up Settings"' + q)
+    os.system('cd ' + pn + ' && git push -u origin master' + q)
     print("  OK")
     print("")
 
@@ -158,22 +203,35 @@ if __name__ == '__main__':
     if standardsite:
         print("* Copying files from Standardsite")
         os.system('git clone ' + DIVO_STANDARDSITE + ' ' + pn + '_tmp/ss' + q)
+        versions['divio-standardsite'] = os.popen('cd ' + pn + '_tmp/ss && git log | head -1').read().strip("commit ").strip("\n")
         os.system('cp ' + pn + '_tmp/ss/templates/standardsite.html ' + pn + '/templates')
         os.system('cp ' + pn + '_tmp/ss/sass/includes/_standardsite.scss ' + pn + '/sass/includes/')
         os.system('cp ' + pn + '_tmp/ss/sass/includes/_settings.scss ' + pn + '/sass/includes/')
         print("  OK")
         print("")
 
+        # after standardsite add push to github
+        print("* Added Standardsite files, another push to Github")
+        os.system('cd ' + pn + ' && git add -A .')
+        os.system('cd ' + pn + ' && git commit -m "djinit (4/' + str(pushes) +
+                  '): Added standardsite files" -m "push based on divio-standardsite (divio/divio-standardsite@'
+                  + str(versions['divio-standardsite']) + ')"' + q)
+        os.system('cd ' + pn + ' && git push -u origin master' + q)
+        print("  OK")
+        print("")
+
     # cleanup
     print("* Cleaning up")
     os.system('rm -rf ' + pn + '_tmp')
+    os.system('rm ' + pn + '/project_init.sh')
     print("  OK")
     print("")
 
-    # push to github
-    print("* Pushing local repo to Github")
-    os.system('cd ' + pn + ' && git add .')
-    os.system('cd ' + pn + ' && git commit -m "Initial commit (djinit)"' + q)
+
+    # after cleanup push to github
+    print("* Cleaned up, another push to Github")
+    os.system('cd ' + pn + ' && git add -A .')
+    os.system('cd ' + pn + ' && git commit -m "djinit (' + str(pushes) + '/' + str(pushes) + '): Cleanup"' + q)
     os.system('cd ' + pn + ' && git push -u origin master' + q)
     print("  OK")
     print("")
