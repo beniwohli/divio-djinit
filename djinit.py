2730997969a45c188158b11daa9fc7353e692eee
Kim Thoenen <kim@smuzey.ch>
2013-09-17 11:50:41 +0200

    heavily untested, dirty code, needs lots of love (major restructuring)

--- djinit.py
+++ djinit.py
@@ -1,24 +1,159 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# djinit - Divio's Django CMS Initializer
-# For Divio-Internal Use only. Usage:
-#  python -c "$(curl -fsSL https://raw.github.com/Chive/playground/master/python/djinit.py)"
-#
-# Copyright (C) 2013 Kim Thoenen <kim@smuzey.ch>.  All Rights Reserved.
 
+"""
+djinit: Divio's DjangoCMS Initializer
+
+Usage:
+    djinit <repo_name> -u <user> -p <pass> [-o <organization> -c -s -m -P -R <remote>]
+    djinit <repo_name> -n -u <user> -p <pass> [-o <organization> -c -s -m -P -R <remote>]
+    djinit <repo_name> -f -u <user> -p <pass> [-o <organization> -m -P -R <remote>]
+    djinit <repo_name> -d -u <user> -p <pass> [-o <organization> -R <remote>]
+    djinit (-h | --help)
+    djinit --version
+
+Options:
+    -n                         Non-Interactive Mode
+    -f                         Fast Mode (includes -c -s)
+    -d                         Divio Mode (includes -c -s -m -P)
+    -c                          create github repo
+    -s                          install standarsite styles
+    -m                          make init
+    -u --user=<user>            Github Username [nope: or Email (defaults to git-config's user.email)]
+    -p --pass=<pass>            Github Password
+    -P --private                Make a private project
+    -R --remote=<remote>        Github Remote Link
+    -o --org=<organization>     create repo on organization account
+"""
 import os
 from random import choice
 import re
 import ask
+from docopt import docopt
 
+
+def setup(args):
+    conf = dict()
+    conf['pushes'] = 4
+
+    if args['-n'] or args['-f'] or args['-d']:
+        conf['project_name'] = args['<repo_name>']
+        conf['github_pass'] = args['--pass']
+        conf['github_org'] = args['--org']
+        conf['project_private'] = args['--private']
+        conf['github_user'] = args['--user']
+
+        #if args['--user']:
+            #conf['github_user'] = args['--user']
+        #else:
+            #conf['github_user'] = os.popen('git config --global user.email').read().strip("\n")
+
+        conf['repo_owner'] = get_repo_owner(conf)
+
+        conf['create_repo'] = args['-c']
+        conf['standardsite'] = args['-s']
+        conf['make_init'] = args['-m']
+
+        # Overrides for fast modes
+        if args['-f'] or args['-d']:
+            conf['standardsite'] = True
+            conf['create_repo'] = True
+
+        if args['-d']:
+            conf['make_init'] = True
+            conf['project_private'] = True
+
+        if conf['create_repo']:
+            conf = create_github_repo(conf)
+
+    else:
+        ask.explain(True)
+        conf['create_repo'] = ask.askBool("Do you want the script to create your Github repo?", default='y')
+
+        if conf['create_repo'] == 'y':
+            conf['create_repo'] = True
+        else:
+            conf['create_repo'] = False
+
+        if conf['create_repo']:
+            conf['github_user'] = ask.ask("Please enter your Github username")
+            conf['github_pass'] = ask.askPassword("Please enter your Github password")
+            conf['github_org'] = ask.ask("If you want to create the repo on an organisation's account, "
+                               "enter its name now. Otherwise hit enter", default='')
+            conf['project_name'] = ask.ask("Please enter a name for the repo")
+            conf['project_private'] = ask.askBool("Should the repo be private?", default='y')
+            conf['repo_owner'] = get_repo_owner(conf)
+            conf = create_github_repo(conf)
+
+        else:
+            print("Please create your repo on Github first then!")
+            conf['project_name'] = ask.ask("Please enter the repo name")
+            conf['repo_owner'] = ask.ask("Please enter the account name which the repo belongs to")
+            i = ask.askBool("Is this the repo's Github remote: git@github.com:" + conf['repo_owner'] + "/"
+                      + conf['project_name'] + ".git?", default='y')
+            if i == 'y':
+                conf['github_remote'] = "git@github.com:" + conf['repo_owner'] + "/" + conf['project_name'] + ".git"
+            elif i == 'n':
+                conf['github_remote'] = ask.ask("Please enter your github remote link:")
+
+        if ask.askBool("Do you want to add the standardsite styles and templates from divio-styleguide?",
+                       default='y') == 'y':
+            conf['standardsite'] = True
+            conf['pushes'] += 1
+        else:
+            conf['standardsite'] = False
+
+        if ask.askBool("Do you want to create a virtualenv and run the makefile (make init)?", default='y') == 'y':
+            conf['make_init'] = True
+        else:
+            conf['make_init'] = False
+
+    return conf
+
+
+def create_github_repo(conf):
+    try:
+        from pyrate.services import github
+    except ImportError:
+        raise ImportError("Warning: Pyrate could not be found."
+                          "Please install it and try again. (pip install pyrate -U)")
+
+    if conf['project_private'] == 'y':
+        conf['project_private'] = True
+
+    elif conf['project_private'] == 'n':
+        conf['project_private'] = False
+
+    h = github.GithubPyrate(conf['github_user'], conf['github_pass'])
+    if conf['github_org']:
+        r = h.create_repo(conf['project_name'], org_name=conf['github_org'], private=conf['project_private'])
+    else:
+        r = h.create_repo(conf['project_name'], private=conf['project_private'])
+
+    #exit("Debug: " + str(r))
+
+    #TODO: check if it actually worked!
+
+    #if r['message'] == 'Validation Failed':
+    #    exit("Error: " + str(r))
+
+    conf['github_remote'] = "git@github.com:" + conf['repo_owner'] + "/" + conf['project_name'] + ".git"
+    return conf
+
+
+def get_repo_owner(conf):
+    if conf['github_org']:
+        return conf['github_org']
+    else:
+        return conf['github_user']
 
 if __name__ == '__main__':
+    conf = setup(docopt(__doc__, version='Djinit 0.0.1'))
+
     DIVIO_DJANGO_TEMPLATE = 'git@github.com:divio/divio-django-template.git'
     DIVIO_BOILERPLATE = 'git@github.com:divio/divio-boilerplate.git'
     DIVO_STANDARDSITE = 'git@github.com:divio/divio-standardsite.git'
     versions = {}
-    pushes = 4
 
     q = " -q"  # quiet flag for git commands
 
@@ -26,66 +161,12 @@ if __name__ == '__main__':
     print("-----------------------------")
     print(" Divio DjangoCMS Initializer")
     print("-----------------------------")
-    ask.explain(True)
-    i = ask.askBool("Do you want the script to create your Github repo?", default='y')
-    if i == 'y':
-        try:
-            from pyrate.services import github
-        except ImportError:
-            raise ImportError("Warning: Pyrate could not be found."
-                              "Please install it and try again. (pip install pyrate -U)")
 
-        github_user = ask.ask("Please enter your Github username")
-        github_pass = ask.askPassword("Please enter your Github password")
-        github_org = ask.ask("If you want to create the repo on an organisation's account, "
-                           "enter its name now. Otherwise hit enter", default='')
-        project_name = ask.ask("Please enter a name for the repo")
-        project_private = ask.askBool("Should the repo be private?", default='y')
-        if project_private == 'y':
-            project_private = True
-
-        elif project_private == 'n':
-            project_private = False
-
-        h = github.GithubPyrate(github_user, github_pass)
-        if github_org:
-            h.create_repo(project_name, org_name=github_org, private=project_private)
-            repo_owner = github_org
-        else:
-            h.create_repo(project_name, private=project_private)
-            repo_owner = github_user
-
-        # TODO: check if it actually worked!
-        github_remote = "git@github.com:" + repo_owner + "/" + project_name + ".git"
-
-    elif i == 'n':
-        print("Please create your repo on Github first then!")
-        project_name = ask.ask("Please enter the repo name")
-        repo_owner = ask.ask("Please enter the account name which the repo belongs to")
-        i = ask.askBool("Is this the repo's Github remote: git@github.com:" + repo_owner + "/"
-                  + project_name + ".git?", default='y')
-        if i == 'y':
-            github_remote = "git@github.com:" + repo_owner + "/" + project_name + ".git"
-        elif i == 'n':
-            github_remote = ask.ask("Please enter your github remote link:")
-
-    i = ask.askBool("Do you want to add the standardsite styles and templates from divio-styleguide?", default='y')
-    if i == 'y':
-        standardsite = True
-        pushes += 1
-    elif i == 'n':
-        standardsite = False
-
-    i = ask.askBool("Do you want to create a virtualenv and run the makefile (make init)?", default='y')
-    if i == 'y':
-        minit = True
-    elif i == 'n':
-        minit = False
 
     print("")
 
     # for ease of use
-    pn = project_name
+    pn = conf['project_name']
 
     # cloning template
     print("* Cloning Divio Django Template")
@@ -99,14 +180,14 @@ if __name__ == '__main__':
     print("* Initializing Git repo")
     os.system('rm -rf ' + pn + '/.git')
     os.system('cd ' + pn + ' && git init' + q)
-    os.system('cd ' + pn + ' && git remote add origin ' + github_remote)
+    os.system('cd ' + pn + ' && git remote add origin ' + conf['github_remote'])
     print("  OK")
     print("")
 
     # initial push to github
     print("* Initial Push to Github")
     os.system('cd ' + pn + ' && git add -A .')
-    os.system('cd ' + pn + ' && git commit -m "djinit (1/' + str(pushes)
+    os.system('cd ' + pn + ' && git commit -m "djinit (1/' + str(conf['pushes'])
               + '): Initial commit" -m "push based on divio-django-template (divio/divio-django-template@'
               + str(versions['divio-django-template']) + ')"' + q)
     os.system('cd ' + pn + ' && git push -u origin master' + q)
@@ -128,7 +209,7 @@ if __name__ == '__main__':
     # after boilerplate add push to github
     print("* Added Boilerplate Files, another push to Github")
     os.system('cd ' + pn + ' && git add -A .')
-    os.system('cd ' + pn + ' && git commit -m "djinit (2/' + str(pushes)
+    os.system('cd ' + pn + ' && git commit -m "djinit (2/' + str(conf['pushes'])
               + '): Added boilerplate files" -m "push based on divio-boilerplate (divio/divio-boilerplate@'
               + str(versions['divio-boilerplate']) + ')"' + q)
     os.system('cd ' + pn + ' && git push -u origin master' + q)
@@ -164,13 +245,13 @@ if __name__ == '__main__':
     # after settings setup push to github
     print("* Set up Settings, another push to Github")
     os.system('cd ' + pn + ' && git add -A .')
-    os.system('cd ' + pn + ' && git commit -m "djinit (3/' + str(pushes) + '): Set up Settings"' + q)
+    os.system('cd ' + pn + ' && git commit -m "djinit (3/' + str(conf['pushes']) + '): Set up Settings"' + q)
     os.system('cd ' + pn + ' && git push -u origin master' + q)
     print("  OK")
     print("")
 
     # adding standardsite styles and templates
-    if standardsite:
+    if conf['standardsite']:
         print("* Copying files from Standardsite")
         os.system('git clone ' + DIVO_STANDARDSITE + ' ' + pn + '_tmp/ss' + q)
         versions['divio-standardsite'] = os.popen('cd ' + pn + '_tmp/ss && git log | head -1').read().strip("commit ").strip("\n")
@@ -183,7 +264,7 @@ if __name__ == '__main__':
         # after standardsite add push to github
         print("* Added Standardsite files, another push to Github")
         os.system('cd ' + pn + ' && git add -A .')
-        os.system('cd ' + pn + ' && git commit -m "djinit (4/' + str(pushes) +
+        os.system('cd ' + pn + ' && git commit -m "djinit (4/' + str(conf['pushes']) +
                   '): Added standardsite files" -m "push based on divio-standardsite (divio/divio-standardsite@'
                   + str(versions['divio-standardsite']) + ')"' + q)
         os.system('cd ' + pn + ' && git push -u origin master' + q)
@@ -201,12 +282,12 @@ if __name__ == '__main__':
     # after cleanup push to github
     print("* Cleaned up, another push to Github")
     os.system('cd ' + pn + ' && git add -A .')
-    os.system('cd ' + pn + ' && git commit -m "djinit (' + str(pushes) + '/' + str(pushes) + '): Cleanup"' + q)
+    os.system('cd ' + pn + ' && git commit -m "djinit (' + str(conf['pushes']) + '/' + str(conf['pushes']) + '): Cleanup"' + q)
     os.system('cd ' + pn + ' && git push -u origin master' + q)
     print("  OK")
     print("")
 
-    if minit:
+    if conf['make_init']:
         print("* Setting up virtual environment")
         os.system('cd ' + pn + ' && virtualenv --prompt="(' + pn + ')" env --no-site-packages -q')
         print("  OK")
@@ -220,7 +301,7 @@ if __name__ == '__main__':
     print("")
     print("")
     print("All done!")
-    if standardsite:
+    if conf['standardsite']:
         print("")
         print("don't forget to:")
         print("* add ('standardsite.html', _('standardsite')),"
